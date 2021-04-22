8ed738bba5e44b3d6538bf8a41f40dff88c3ca9e
SOLRAK77 <ccastillo.perez77@gmail.com>
2021-04-22 11:44:52 -0500

    Colocano el router secundario, parte II

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,24 +2,20 @@ import React from "react";
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 import { LoginScreen } from "../components/login/LoginScreen";
-import { MarvelScreen } from "../components/marvel/MarvelScreen";
-import { Navbar } from "../components/ui/NavBar";
+import { DashboardRoutes } from "./DashboardRoutes";
 
 export default function AppRouter() {
   return (
     <Router>
       <div>
-        <Navbar />
-
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
           <Route exact path="/login" component={LoginScreen} /> 
-          <Route exact path="/" component={MarvelScreen} />             
+          <Route path="/" component={DashboardRoutes} />             
         </Switch>
       </div>
     </Router>
---
+++ src/routers/DashboardRoutes.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Navbar } from '../components/ui/NavBar'
+import {
+    Switch,
+    Route,
+    Redirect
+  } from "react-router-dom";
+import { MarvelScreen } from '../components/marvel/MarvelScreen';
+import { HeroScreen } from '../components/heroes/HeroScreen';
+import { DcScreen } from '../components/dc/DcScreen';
+
+export const DashboardRoutes = () => {
+    return (
+        <>
+          <Navbar/>  
+          <div>
+              <Switch>
+                <Route exact path="/marvel" component={MarvelScreen} />
+                <Route exact path="/heroes/:heroeId" component={HeroScreen} />
+                <Route exact path="/dc" component={DcScreen} />
+                {/* Sinocumple ninguna de las opciones manda a: */}
+                <Redirect to="/marvel" />
+              </Switch>
+          </div>
+        </>
+    )
+}
