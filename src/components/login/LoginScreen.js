93477c5d71f7eb73ccb99c97a4192c62f93cbadf
SOLRAK77 <ccastillo.perez77@gmail.com>
2021-04-22 12:02:03 -0500

    diferencias entre push/replace en el history

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
-export const LoginScreen = () => {
+export const LoginScreen = ({history}) => {
+
+    const handleAccesar = () => {
+        // history.push('/');
+        // La diferencia es que reemplaza el historia del nevagador
+        history.replace('/');
+    }
+
     return (
-        <div>
+        <div className="container mt-5">
             <h1>Login Screen</h1>
+            <hr/>
+            <button
+                className="btn btn-primary"
+                onClick={handleAccesar}
+            >
+                Accesar
+            </button>
         </div>
     )
 }
